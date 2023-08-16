6740e5d092eb29147c0e40805092b2642b0af46d
Lucianjoki <luciawaweru22@gmail.com>
2023-08-16 08:14:30 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,13 +7,12 @@
 
 int main(void)
 {
-	int sum = 0
-	int i;
+	int i, sum = 0;
 
 	for (i = 0 ; i < 1024 ; i++)
 	{
-		if (i % 3 == 0 || i % 5 == 0)
-			sum = sum + i;
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
 	}
 
 	printf("%d\n", sum);
