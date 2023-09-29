6b9364f758f77f7489398c1d6c785536af0da44d
Lucianjoki <luciawaweru22@gmail.com>
2023-09-29 10:54:07 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,12 +19,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-	}
-	for (a = 0; b[a] != '0'; a++)
-	{
-		num <<= 1;
-		if (b[a] == '1')
-			num += 1;
+		num = (2 *num + b[i] - '0');
 	}
 	return (num);
 }
