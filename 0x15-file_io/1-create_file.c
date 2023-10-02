ccade5ab7605d0f7998acd4e3b7e220ded358991
Lucianjoki <luciawaweru22@gmail.com>
2023-10-02 12:42:15 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	int mletters;
 	int rwr;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
 	file_k = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
